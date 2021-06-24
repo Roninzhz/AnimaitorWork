de80cf853b4a9e235fee14f9938d8828e8522e2f
1938917626@qq.com <1938917626@qq.com>
2021-06-24 11:36:09 +0800

    周一作业

---
+++ Assets/Scripts/AppExit.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppExit : MonoBehaviour {
+
+	public void GameOver()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
